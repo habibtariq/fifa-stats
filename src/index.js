73116558b03d18e90b94adf6be32db9a160838c9
Habib Tariq <ht752@nyu.edu>
2017-05-09 21:19:02 -0400

    setting up the project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { SkiDayCount} from './components/SkiDayCount'
 
 render(
-	<div>
-	</div>,
+	<SkiDayCount />,
 	document.getElementById('react-container')
 )
